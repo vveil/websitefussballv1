af02f620a93cbd456cbe7e001d1fd98ef3211d15
Niklas Werner <vveil.socialmedia@gmail.com>
2020-07-26 13:10:58 +0200

    added component for formular

---
+++ src/components/home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="text">
+      hallo hallo
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.text {
+  background-color: #000000;
+}
+</style>
