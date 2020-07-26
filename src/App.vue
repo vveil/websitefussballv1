526f8d9a3026fd4c44abd1d25dca96bc735d851c
Niklas Werner <vveil.socialmedia@gmail.com>
2020-07-26 13:10:05 +0200

    deleted standard vue temlates etc

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <home msg="Ja moin"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import home from './components/home.vue'
+import formular from './components/formular.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    home,
+    formular
   }
 }
 </script>
